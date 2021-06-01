3caddf9b5476f4d34525b350cdcacf5876522314
samho <skaamrheon00211301@gmail.com>
2021-06-02 07:28:51 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/gitproject2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gitproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+    How to Use Git and GitHub in Android Studio
+    https://www.youtube.com/watch?v=ebqbIyicCFU
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
